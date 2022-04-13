a1469827026dacf8449b75c079b008bb51edd589
Christopher Lynde <lyndechris@gmail.com>
2022-04-13 14:29:58 -0400

    Collapse TaskGroup Icons
    -Added ArrowDrop icons to TaskGroup
    -Separated TaskGroup into collapsed/expanded templates

--- src/TaskGroup.js
+++ src/TaskGroup.js
@@ -1,8 +1,12 @@
 import React, { useState } from "react";
 import Task from "./Task";
-import "./css/TaskView.css";
+import "./css/TaskGroup.css";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import ArrowDropUpIcon from "@mui/icons-material/ArrowDropUp";
 
 function TaskGroup(props) {
+	const [isCollapsed, setCollapsed] = useState(props.collapsed);
+
 	const tasks = props.tasks
 		.filter((task) => task.category === props.name)
 		.map((task) => (
@@ -21,13 +25,35 @@ function TaskGroup(props) {
 		props.name.slice(0, 1).toUpperCase() +
 		props.name.slice(1, props.name.length);
 
-	return (
+	//create 2 taskgroup templates for collapsed/expanded and add uparrow/downarrow icons
+	//add onhover point cursor
+	const collapsedTemplate = (
 		<div className="TaskGroup">
-			<h2 className={props.name} onClick={props.handleGroups}>
-				{name}
-			</h2>
-			<ul className={props.collapsed ? "collapsed" : "expanded"}>{tasks}</ul>
+			<div className="taskgroup-header" onClick={props.handleGroups}>
+				<h2 className={props.name}>{name}</h2>
+				<ArrowDropDownIcon />
+			</div>
 		</div>
+	);
+
+	const expandedTemplate = (
+		<div className="TaskGroup">
+			<div className="taskgroup-header" onClick={props.handleGroups}>
+				<h2 className={props.name}>{name}</h2>
+				<ArrowDropUpIcon />
+			</div>
+			<ul className={"expanded"}>{tasks}</ul>
+		</div>
+	);
+
+	return (
+		// <div className="TaskGroup">
+		// 	<h2 className={props.name} onClick={props.handleGroups}>
+		// 		{name}
+		// 	</h2>
+		// 	<ul className={props.collapsed ? "collapsed" : "expanded"}>{tasks}</ul>
+		// </div>
+		<>{isCollapsed ? collapsedTemplate : expandedTemplate}</>
 	);
 }
 
