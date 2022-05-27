ebab9ef9a6a46c1786f4b2d8557a914871788131
LyndeCS <lyndechris@gmail.com>
2022-05-27 10:59:39 -0400

    Update Task.js

--- src/Task.js
+++ src/Task.js
@@ -1,22 +1,51 @@
 import React, { useState } from "react";
 import "./css/Task.css";
-import { Checkbox, TextField, IconButton, Button } from "@mui/material";
-import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
+import {
+	Checkbox,
+	TextField,
+	IconButton,
+	Button,
+	Modal,
+	Typography,
+	Box,
+} from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
 import ScheduleIcon from "@mui/icons-material/Schedule";
-import DateFnsUtils from "@date-io/date-fns"; //fixme: redundant import below
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { TimePicker } from "@mui/x-date-pickers/TimePicker";
+
+//fixme: refactor
+const style = {
+	position: "absolute",
+	top: "50%",
+	left: "50%",
+	transform: "translate(-50%, -50%)",
+	width: 400,
+	bgcolor: "background.paper",
+	border: "2px solid #000",
+	boxShadow: 24,
+	p: 4,
+};
 
 function Task(props) {
 	// add task button or double click should be only way to edit, one task at a time
 	const [newDesc, setNewDesc] = useState(props.desc);
 	const [isEditing, setEditing] = useState(props.desc ? false : true);
 
+	// modal
+	const [isModalOpen, setModalOpen] = useState(false);
+	const handleModalOpen = () => setModalOpen(true);
+	const handleModalClose = () => setModalOpen(false);
+
 	// fixme: DatePicker separated from Task.js
+	let currDate = Date.now();
 	const [isDateTimePickerOpen, setIsDateTimePickerOpen] = useState(false);
-	const [selectedDate, setSelectedDate] = useState(new Date());
+	const [selectedDate, setSelectedDate] = useState(currDate);
+	const [startTime, setStartTime] = useState(currDate);
+	const [endTime, setEndTime] = useState(currDate + 1 * 60 * 60 * 1000);
 
 	function handleChange(e) {
 		setNewDesc(e.target.value);
@@ -123,39 +152,43 @@ function Task(props) {
 				{props.desc}
 			</div>
 			<div className="task-button-container">
-				<LocalizationProvider dateAdapter={AdapterDateFns}>
-					<DateTimePicker
-						open={isDateTimePickerOpen}
-						onClose={handleDateTimePickerClose}
-						value={selectedDate}
-						onChange={setSelectedDate}
-						renderInput={({
-							ref,
-							inputProps,
-							disabled,
-							onChange,
-							value,
-							...other
-						}) => (
-							<div ref={ref} {...other}>
-								<input
-									style={{ display: "none" }}
-									value={value}
-									onChange={onChange}
-									disabled={disabled}
-									{...inputProps}
-								/>
-								<IconButton
-									aria-label="schedule"
-									className="schedule-button"
-									onClick={() => setIsDateTimePickerOpen((isOpen) => !isOpen)}
-								>
-									<ScheduleIcon className="schedule-icon" />
-								</IconButton>
-							</div>
-						)}
-					/>
-				</LocalizationProvider>
+				<IconButton
+					aria-label="schedule"
+					className="schedule-button"
+					onClick={handleModalOpen}
+				>
+					<ScheduleIcon className="schedule-icon" />
+				</IconButton>
+				<Modal open={isModalOpen} onClose={handleModalClose}>
+					<Box sx={style}>
+						<LocalizationProvider dateAdapter={AdapterDateFns}>
+							<DatePicker
+								label="Select Date"
+								value={selectedDate}
+								onChange={(newValue) => {
+									setSelectedDate(newValue);
+								}}
+								renderInput={(params) => <TextField {...params} />}
+							/>
+							<TimePicker
+								label="Start Time"
+								value={startTime}
+								onChange={(newValue) => {
+									setStartTime(newValue);
+								}}
+								renderInput={(params) => <TextField {...params} />}
+							/>
+							<TimePicker
+								label="End Time"
+								value={endTime}
+								onChange={(newValue) => {
+									setEndTime(newValue);
+								}}
+								renderInput={(params) => <TextField {...params} />}
+							/>
+						</LocalizationProvider>
+					</Box>
+				</Modal>
 				<IconButton
 					aria-label="delete"
 					className="delete-button"
