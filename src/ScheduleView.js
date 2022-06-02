08871f3ff601bf9d2afd7ec57941874c85fda0ab
Christopher Lynde <lyndechris@gmail.com>
2022-06-02 19:11:03 -0400

    TimeIndicator
    -Added TimeIndicator to Schedule View

--- src/ScheduleView.js
+++ src/ScheduleView.js
@@ -8,11 +8,49 @@ import {
 	DateNavigator,
 	Appointments,
 	TodayButton,
+	CurrentTimeIndicator,
 } from "@devexpress/dx-react-scheduler-material-ui";
 import "./css/ScheduleView.css";
+import classNames from "clsx";
+import { styled, alpha } from "@mui/material/styles";
 
 const currentDate = new Date();
 const currentHour = currentDate.getHours();
+
+//fixme: devexpress example
+const PREFIX = "Demo";
+const classes = {
+	line: `${PREFIX}-line`,
+	circle: `${PREFIX}-circle`,
+	nowIndicator: `${PREFIX}-nowIndicator`,
+	shadedCell: `${PREFIX}-shadedCell`,
+	shadedPart: `${PREFIX}-shadedPart`,
+	appointment: `${PREFIX}-appointment`,
+	shadedAppointment: `${PREFIX}-shadedAppointment`,
+};
+const StyledDiv = styled("div", {
+	shouldForwardProp: (prop) => prop !== "top",
+})(({ theme, top }) => ({
+	[`& .${classes.line}`]: {
+		height: "2px",
+		borderTop: `2px black solid`,
+		width: "100%",
+		transform: "translate(0, -1px)",
+	},
+	[`& .${classes.circle}`]: {
+		width: theme.spacing(1.5),
+		height: theme.spacing(1.5),
+		borderRadius: "50%",
+		transform: "translate(-50%, -50%)",
+		background: "black",
+	},
+	[`& .${classes.nowIndicator}`]: {
+		position: "absolute",
+		zIndex: 1,
+		left: 0,
+		top,
+	},
+}));
 
 function ScheduleView(props) {
 	const schedulerData = props.tasks.map((task) => {
@@ -23,18 +61,12 @@ function ScheduleView(props) {
 		};
 	});
 
-	// const schedulerData = [
-	// 	{
-	// 		startDate: currentDate,
-	// 		endDate: currentDate,
-	// 		title: "Meeting",
-	// 	},
-	// 	{
-	// 		startDate: currentDate,
-	// 		endDate: currentDate,
-	// 		title: "Go to a gym",
-	// 	},
-	// ];
+	const TimeIndicator = ({ top, ...restProps }) => (
+		<StyledDiv top={top} {...restProps}>
+			<div className={classNames(classes.nowIndicator, classes.circle)} />
+			<div className={classNames(classes.nowIndicator, classes.line)} />
+		</StyledDiv>
+	);
 
 	return (
 		<div className="schedule-container">
@@ -45,6 +77,7 @@ function ScheduleView(props) {
 				<DateNavigator />
 				<TodayButton />
 				<Appointments />
+				<CurrentTimeIndicator indicatorComponent={TimeIndicator} />
 			</Scheduler>
 		</div>
 	);
